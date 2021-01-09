1b8de180760540667cb9cdfa48bfb7499fe2934d
Aykut Saki <aykutsakisocial@gmail.com>
2021-01-09 15:35:23 +0300

    deleted all backend files

--- server/app.js
+++ server/app.js
@@ -3,17 +3,11 @@ import mongoose from "mongoose";
 import cors from "cors";
 import "dotenv/config.js";
 
-import posts from "./routers/posts.js";
-import users from "./routers/users.js";
-
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors());
-
-app.use("/posts", posts);
-app.use("/users", users)
 
 mongoose
   .connect(process.env.DB_CONNECTION_URI, {

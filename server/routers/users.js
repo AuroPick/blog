d47147da3deb5f9df81986f61743141213fbba3a
Aykut Saki <aykutsakisocial@gmail.com>
2021-01-09 21:26:43 +0300

    fixed users route

--- server/routers/users.js
+++ server/routers/users.js
@@ -1,8 +1,33 @@
-import { Router as router} from "express";
-import { register, login, logout, admin, authenticated } from "../controllers/users.js";
+import express from "express";
+import passport from "passport";
+import "../config/passport.js";
+
+import {
+  register,
+  login,
+  logout,
+  admin,
+  authenticated,
+} from "../controllers/users.js";
+
+const router = express.Router();
 
 router.post("/register", register);
-router.post("/login", login);
-router.get("/logout", logout);
-router.get("/admin", admin);
-router.get("/authenticated", authenticated);
+
+router.post(
+  "/login",
+  passport.authenticate("local", { session: false }),
+  login
+);
+
+router.get("/logout", passport.authenticate("jwt", { session: false }), logout);
+
+router.get("/admin", passport.authenticate("jwt", { session: false }), admin);
+
+router.get(
+  "/authenticated",
+  passport.authenticate("jwt", { session: false }),
+  authenticated
+);
+
+export default router;

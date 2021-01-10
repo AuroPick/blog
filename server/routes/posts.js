782ecadbc329d075e63ce8726c4ec101147eb73f
Aykut Saki <aykutsakisocial@gmail.com>
2021-01-10 18:07:49 +0300

    updated post route

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -24,13 +24,32 @@ router.get(
         res.locals.authenticated = false;
         next();
       } else {
-          res.locals.authenticated = true;
-          next();}
+        res.locals.authenticated = true;
+        next();
+      }
     })(req, res, next);
   },
   getPosts
 );
 
-router.get("/post", getPost);
+router.get(
+  "/post/:id",
+  (req, res, next) => {
+    passport.authenticate("jwt", { session: false }, (err, user, info) => {
+      if (err)
+        res
+          .status(500)
+          .json({ message: { msg: "Bir hata oluştu", msgError: true } });
+      if (!user) {
+        res.locals.authenticated = false;
+        next();
+      } else {
+        res.locals.authenticated = true;
+        next();
+      }
+    })(req, res, next);
+  },
+  getPost
+);
 
 export default router;

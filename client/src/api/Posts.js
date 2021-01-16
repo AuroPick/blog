a9c20c3238b8ddd0ad35ea878e6a6952e93dd6f3
Aykut Saki <aykutsakisocial@gmail.com>
2021-01-17 01:47:56 +0300

    added api calls

---
+++ client/src/api/Posts.js
@@ -0,0 +1,64 @@
+import axios from "axios";
+
+const url = "http://localhost:5000";
+
+export const createPost = async ({ title, message, creator, specialPost }) => {
+  try {
+    const createPost = await axios.post(
+      `${url}/posts/create`,
+      {
+        title,
+        message,
+        creator,
+        specialPost,
+      },
+      {
+        withCredentials: true,
+        credentials: "include",
+      }
+    );
+
+    return createPost;
+  } catch (error) {
+    return { message: { msg: "Bir hata oluştu", msgError: true } };
+  }
+};
+
+export const getPosts = async () => {
+  try {
+    const getPosts = await axios.get(`${url}/posts/posts`, {
+      withCredentials: true,
+      credentials: "include",
+    });
+
+    return getPosts;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const deletePost = async (id) => {
+  try {
+    const deletePost = await axios.delete(`${url}/posts/delete/${id}`, {
+      withCredentials: true,
+      credentials: "include",
+    });
+
+    return deletePost;
+  } catch (error) {
+    return { data: { message: { msg: "Bir hata oluştu", msgError: true } } };
+  }
+};
+
+export const updatePost = async (id, post) => {
+  try {
+    const updatePost = await axios.patch(`${url}/posts/update/${id}`, post, {
+      withCredentials: true,
+      credentials: "include",
+    });
+
+    return updatePost;
+  } catch (error) {
+    return { data: { message: { msg: "Bir hata oluştu", msgError: true } } };
+  }
+};
---
+++ client/src/api/Users.js
@@ -0,0 +1,104 @@
+import axios from "axios";
+
+const url = "http://localhost:5000";
+
+export const register = async ({ email, username, password }) => {
+  try {
+    const register = await axios.post(`${url}/users/register`, {
+      email,
+      username,
+      password,
+    });
+
+    return register;
+  } catch (error) {
+    return {
+      data: {
+        message: { msg: "Bir hata oluştu", msgError: true },
+        isAuthenticated: false,
+      },
+    };
+  }
+};
+
+export const login = async ({ username, password }) => {
+  try {
+    const login = await axios.post(
+      `${url}/users/login`,
+      {
+        username,
+        password,
+      },
+      { withCredentials: true, credentials: "include" }
+    );
+
+    return login;
+  } catch (error) {
+    return {
+      message: { msg: "Bir hata oluştu", msgError: true },
+      data: { isAuthenticated: false },
+    };
+  }
+};
+
+export const logout = async () => {
+  try {
+    const logout = await axios.get(`${url}/users/logout`, {
+      withCredentials: true,
+      credentials: "include",
+    });
+
+    return logout;
+  } catch (error) {
+    return { message: { msg: "Bir hata oluştu", msgError: true } };
+  }
+};
+
+export const admin = async () => await axios.get(`${url}/users/admin`);
+
+export const isAuthenticated = async () => {
+  try {
+    const isAuthenticated = await axios.get(`${url}/users/authenticated`, {
+      withCredentials: true,
+      credentials: "include",
+    });
+
+    return isAuthenticated;
+  } catch (error) {
+    return {
+      message: { msg: "Bir hata oluştu", msgError: true },
+      data: { isAuthenticated: false },
+    };
+  }
+};
+
+export const getUsers = async () => {
+  try {
+    const getUsers = await axios.get(`${url}/users/get`, {
+      withCredentials: true,
+      credentials: "include",
+    });
+
+    return {
+      getUsers,
+      message: { msg: "Kullanıcılar getirildi", msgError: false },
+    };
+  } catch (error) {
+    return {
+      message: { msg: "Bir hata oluştu", msgError: true },
+    };
+  }
+};
+
+export const deleteUser = async (id) => {
+  try {
+    const deleteUser = await axios.delete(`${url}/users/delete/${id}`, {
+      withCredentials: true,
+      credentials: "include",
+    });
+
+    return deleteUser;
+  } catch (error) {
+    return { message: { msg: "Bir hata oluştu", msgError: true } };
+  }
+};

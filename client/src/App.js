bcf503774f4c22bfc406d53b1b35b45f59d765e1
Aykut Saki <aykutsakisocial@gmail.com>
2021-01-17 01:49:44 +0300

    updated App.js and index.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
+
+import Index from "./components";
+import PostDetail from "./components/PostDetail/PostDetail";
+import Users from "./components/Users/Users";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/" exact component={Index} />
+        <Route path="/post/:_id" exact children={<PostDetail />} />
+        <Route path="/users" exact component={Users} />
+        <Route path="*" component={Index} />
+      </Switch>
+    </Router>
   );
 }
 

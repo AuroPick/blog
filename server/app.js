7fa7ab10619ad7f6fddf402c23665ee5d5367abe
Aykut Saki <aykutsakisocial@gmail.com>
2021-01-17 01:41:54 +0300

    updated app.js

--- server/app.js
+++ server/app.js
@@ -10,9 +10,9 @@ import posts from "./routes/posts.js";
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(cors({ origin: true, credentials: true }));
 app.use(cookieParser());
 app.use(express.json());
-app.use(cors());
 
 app.use("/users", users);
 app.use("/posts", posts);
